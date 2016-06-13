7c419d5947c26789a9f6f935bf22ceb14ec18805
hds <hdszju@gmail.com>
2016-06-13 11:21:49 +0800

    add auth

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+// Authenticator
+app.use(express.basicAuth(function(user, pass) {
+ return user === 'testUser' && pass === 'testPass';
+}));
+
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
